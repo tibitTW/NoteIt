0f0ec3bcf904df5221794eb1df5f97b30de3ec3f
TibitFromTaiwan <jim870718@gmail.com>
2019-12-14 00:36:14 +0800

    外觀優化

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 list_count = 0
-document.getElementById('create-list-btn').addEventListener('click', () => {
-    list_string = document.getElementById('list-string')
-    list_string.style.display = 'inline'
-    list_string.focus()
-    document.querySelector('#list-string').addEventListener('keypress', function (e) {
+document.getElementById('create-block-btn').addEventListener('click', () => {
+    insert_string = document.getElementById('insert-string')
+    insert_string.style.display = 'block'
+    insert_string.focus()
+    document.querySelector('#insert-string').addEventListener('keypress', function (e) {
         var key = e.which || e.keyCode
         if (key === 13) { // 13 is enter
-            if (list_string.value === '') {
-                list_string.value = ''
-                list_string.style.display = 'none'
+            if (insert_string.value === '') {
+                insert_string.value = ''
+                insert_string.style.display = 'none'
             } else {
-                console.log(list_string.value)
-                list_string.style.display = 'none'
-                node = document.createElement('p')
-                node.setAttribute('id', 'p'+String(list_count+1))
-                node.textContent = list_string.value
-                document.getElementById('note-block').appendChild(node)
+                note_block = document.getElementById('note-block')
+                insert_block = document.createElement('div')
+                insert_block.textContent = insert_string.value
+                note_block.insertBefore(insert_block, note_block.lastElementChild)
                 
-                list_string.value = ''
+                insert_string.style.display = 'none'
+                insert_string.value = ''
                 list_count++
             }
         }
     })
+})
+
+document.getElementById('insert-string').addEventListener('focusout', () => {
+    insert_string = document.getElementById('insert-string')
+    insert_string.style.display = 'none'
+    insert_string.value = ''
 })
